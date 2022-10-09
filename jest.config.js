07c2a812665d89011478a9b97eae57746ecef844
Roman Tuomisto <romantuomisto@gmail.com>
2022-10-09 23:01:51 +0300

    adds tests for campgrounds model

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import('jest').Config} */
 const config = {
     verbose: true,
+    globalSetup: '<rootDir>/models/tests/globalSetup.js',
+    globalTeardown: '<rootDir>/models/tests/globalTeardown.js',
+    setupFilesAfterEnv: ['<rootDir>/models/tests/setupFile.js'],
 };
 
 module.exports = config;
