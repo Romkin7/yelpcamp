961acd2c8470a3ae3231dd3fcff8912c5d6cc348
Roman Tuomisto <romantuomisto@gmail.com>
2022-10-11 00:30:46 +0300

    adds edit campground tests

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('./config/dbConnection');
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverRide = require('method-override');
 // require routes
 const campgroundsRoutes = require('./routes/campgrounds.routes');
 //Set ip and port
@@ -15,11 +16,12 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverRide('_method'));
 app.locals.appTitle = 'YelpCamp';
 // use routes
 app.use(campgroundsRoutes);
 // Listen on port 8080
-app.listen(8080, () => {
+const server = app.listen(8080, () => {
     // eslint-disable-next-line no-console
     console.info(
         `YelpCamp server staarted on ip ${app.get('ip')} and port ${app.get(
@@ -28,4 +30,4 @@ app.listen(8080, () => {
     );
 });
 
-module.exports = app;
+module.exports = server;
