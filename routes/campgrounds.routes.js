23ad4e66bc93b72fc6d400fb827ce3fdaecb8592
Roman Tuomisto <romantuomisto@gmail.com>
2022-10-10 21:40:45 +0300

    fix eslint error

--- routes/campgrounds.routes.js
+++ routes/campgrounds.routes.js
@@ -35,7 +35,6 @@ router.post('/campgrounds', async (request, response) => {
 router.get('/campgrounds/:id', async (request, response) => {
     try {
         const campground = await Campground.findById(request.params.id);
-        console.log(campground);
         return response.render('campgrounds/show', { campground });
     } catch (error) {
         return response.send(error);
